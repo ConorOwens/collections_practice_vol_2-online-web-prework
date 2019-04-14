6562a619363bdcc8c6f61b4c6bd5d9f968e6a513
Conor Owens <conorj.owens@gmail.com>
2019-04-14 17:23:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(array)
+  count = 0
+  array.each do |value|
+    if value[0].downcase == "r"
+      count += 1
+    end
+  end
+  count == array.size
+end
+
+def contain_a(array)
+  ans = []
+  array.each do |value|
+    if value.include?("a")
+      ans << value
+    end
+  end
+  ans
+end
